63039ed70aebf0984209e6df8357846757e427a4
naru <kmykirby@yahoo.co.jp>
2018-11-29 22:59:24 +0900

    微修正4

--- google_calender.py
+++ google_calender.py
@@ -10,6 +10,19 @@ import os
 # If modifying these scopes, delete the file token.json.
 SCOPES = 'https://www.googleapis.com/auth/calendar'
 
+google_keys = {
+    "type": "service_account",
+    "project_id": os.environ['GOOGLE_SERVICE_PROJECT_ID'],
+    "private_key_id": os.environ['GOOGLE_SERVICE_PRIVATE_KEY_ID'],
+    "private_key": os.environ['GOOGLE_SERVICE_PRIVATE_KEY'],
+    "client_email": os.environ['GOOGLE_SERVICE_CLIENT_EMAIL'],
+    "client_id": os.environ['GOOGLE_SERVICE_CLIENT_ID'],
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": os.environ['GOOGLE_SERVICE_CLIENT_X509_CERT_URL']
+}
+
 
 def get_schedule(calendar_id="primary"):
     """Shows basic usage of the Google Calendar API.
@@ -23,18 +36,7 @@ def get_schedule(calendar_id="primary"):
     # if not creds or creds.invalid:
     #     flow = client.flow_from_clientsecrets('credentials.json', SCOPES)
     #     creds = tools.run_flow(flow, store)
-    google_keys = {
-        "type": "service_account",
-        "project_id": os.environ['GOOGLE_SERVICE_PROJECT_ID'],
-        "private_key_id": os.environ['GOOGLE_SERVICE_PRIVATE_KEY_ID'],
-        "private_key": os.environ['GOOGLE_SERVICE_PRIVATE_KEY'],
-        "client_email": os.environ['GOOGLE_SERVICE_CLIENT_EMAIL'],
-        "client_id": os.environ['GOOGLE_SERVICE_CLIENT_ID'],
-        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-        "token_uri": "https://oauth2.googleapis.com/token",
-        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-        "client_x509_cert_url":  os.environ['GOOGLE_SERVICE_CLIENT_X509_CERT_URL']
-     }
+
     creds = ServiceAccountCredentials.from_json_keyfile_dict(
         google_keys,
         scopes=SCOPES
@@ -85,8 +87,8 @@ def set_schedule(
     if not body.get("end"):
         print("# set_schedule: no 'end' in body")
         return False
-    creds = ServiceAccountCredentials.from_json_keyfile_name(
-        "google_key.json",
+    creds = ServiceAccountCredentials.from_json_keyfile_dict(
+        google_keys,
         scopes=SCOPES
     )
     service = build('calendar', 'v3', http=creds.authorize(Http()))
