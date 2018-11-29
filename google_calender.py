be8b22c737827ec655f8531ddbc3309bff22aea3
naru <kmykirby@yahoo.co.jp>
2018-11-29 22:54:45 +0900

    認証情報取得元をfile->dictに変更

--- google_calender.py
+++ google_calender.py
@@ -5,6 +5,8 @@ from httplib2 import Http
 # from oauth2client import file, client, tools
 from oauth2client.service_account import ServiceAccountCredentials
 
+import os
+
 # If modifying these scopes, delete the file token.json.
 SCOPES = 'https://www.googleapis.com/auth/calendar'
 
@@ -21,9 +23,20 @@ def get_schedule(calendar_id="primary"):
     # if not creds or creds.invalid:
     #     flow = client.flow_from_clientsecrets('credentials.json', SCOPES)
     #     creds = tools.run_flow(flow, store)
-    # (google_key.json: My Project-bca3e612fcba.json)
+    google_keys = {
+        "type": "service_account",
+        "project_id": os.environ['GOOGLE_SERVICE_PROJECT_ID'],
+        "private_key_id": os.environ['GOOGLE_SERVICE_PRIVATE_KEY_ID'],
+        "private_key": os.environ['GOOGLE_SERVICE_PRIVATE_KEY'],
+        "client_email": os.environ['GOOGLE_SERVICE_CLIENT_EMAIL'],
+        "client_id": os.environ['GOOGLE_SERVICE_CLIENT_ID'],
+        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+        "token_uri": "https://oauth2.googleapis.com/token",
+        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+        "client_x509_cert_url":  os.environ['GOOGLE_SERVICE_CLIENT_X509_CERT_URL']
+     }
     creds = ServiceAccountCredentials.from_json_keyfile_name(
-        "google_key.json",
+        google_keys,
         scopes=SCOPES
     )
     service = build('calendar', 'v3', http=creds.authorize(Http()))
